1bcaf4e0e63f2648a98ba97ae0c5c96d03418958
Yusuf Eka Wicaksana <ekayusuf.wicaksana@gmail.com>
2025-10-10 19:36:18 +0700

    feat: hitung luas lingkaran

---
+++ LuasLingkaran.php
@@ -0,0 +1,14 @@
+<?php
+
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 5;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+
+echo "Hasilnya adalah: ". $rumus;
+
